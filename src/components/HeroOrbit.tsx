ad72fcd6b548bda11b4bcffbca10d4b0f4d1cd97
SimonM <simomor@gmail.com>
2024-12-18 21:11:35 +0200

    hero Home page little refactor

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,9 +1,11 @@
 import React, { PropsWithChildren } from "react";
 import StarIcon from "@/assets/icons/star.svg";
 
-type HeroOrbitProps = PropsWithChildren<{ size: number; rotation: number }>;
+export type HeroOrbitProps = PropsWithChildren<{
+	size: number;
+	rotation: number;
+}>;
 
-// todo: continue => 1:10:49
 export const HeroOrbit: React.FC<HeroOrbitProps> = ({
 	children,
 	size,
@@ -13,7 +15,7 @@ export const HeroOrbit: React.FC<HeroOrbitProps> = ({
 		<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
 			{/* animate-spin  [animation-duration:30s]*/}
 			<div
-				className=""
+				className="flex items-start justify-start"
 				style={{
 					transform: `rotate(${rotation}deg)`,
 					height: `${size}px`,
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,49 +3,128 @@ import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
-import { HeroOrbit } from "@/components/HeroOrbit";
+import { HeroOrbit, HeroOrbitProps } from "@/components/HeroOrbit";
 import SparkleIcon from "@/assets/icons/sparkle.svg";
+import clsx from "clsx";
+
+type HeroOrbitMapProps = {
+	child: React.ReactNode;
+} & Pick<HeroOrbitProps, "size" | "rotation">;
+
+const heroOrbitStarMap: HeroOrbitMapProps[] = [
+	{
+		size: 800,
+		rotation: -72,
+		child: <StarIcon className="size-28 text-emerald-300" />
+	},
+	{
+		child: <StarIcon className="size-12 text-emerald-300" />,
+		size: 550,
+		rotation: 20
+	},
+	{
+		size: 590,
+		rotation: 98,
+		child: <StarIcon className="size-8 text-emerald-300" />
+	}
+];
+
+const heroOrbitSparkMap: HeroOrbitMapProps[] = [
+	{
+		size: 430,
+		rotation: -14,
+		child: <SparkleIcon className="size-8 text-emerald-300/20" />
+	},
+	{
+		size: 440,
+		rotation: 79,
+		child: <SparkleIcon className="size-5 text-emerald-300/20" />
+	},
+	{
+		size: 530,
+		rotation: 178,
+		child: <SparkleIcon className="size-10 text-emerald-300/20" />
+	},
+	{
+		size: 710,
+		rotation: 144,
+		child: <SparkleIcon className="size-14 text-emerald-300/20" />
+	}
+];
+
+const heroOrbitDotMap: HeroOrbitMapProps[] = [
+	{
+		size: 720,
+		rotation: 85,
+		child: <div className="size-3 rounded-full bg-emerald-300/20" />
+	},
+	{
+		size: 520,
+		rotation: -41,
+		child: <div className="size-3 rounded-full bg-emerald-300/20" />
+	},
+	{
+		size: 650,
+		rotation: -5,
+		child: <div className="size-3 rounded-full bg-emerald-300/20" />
+	}
+];
 
 export const HeroSection = () => {
 	return (
 		<div className="relative z-0 overflow-x-clip py-32 md:py-48 lg:py-60">
 			<div
-				className="absolute inset-0 -z-30 opacity-5"
+				className="absolute inset-0"
 				style={{
-					backgroundImage: `url(${grainImage.src})`
+					maskImage:
+						"linear-gradient(to bottom, transparent, black 10%, black 70%, transparent)",
+					WebkitMaskImage:
+						"linear-gradient(to bottom, transparent, black 10%, black 70%, transparent)" // For Safari
 				}}
-			></div>
-			<div className="hero-ring size-[620px]"></div>
-			<div className="hero-ring size-[820px]"></div>
-			<div className="hero-ring size-[1020px]"></div>
-			<div className="hero-ring size-[1220px]"></div>
+			>
+				<div
+					className="absolute inset-0 -z-30 opacity-5"
+					style={{
+						backgroundImage: `url(${grainImage.src})`
+					}}
+				></div>
+				{[620, 820, 1020, 1220].map((width) => (
+					<div
+						key={width}
+						className={clsx(["hero-ring", `size-[${width}px]`])}
+					></div>
+				))}
 
-			<HeroOrbit size={800} rotation={-72}>
-				<StarIcon className="size-28 text-emerald-300" />
-			</HeroOrbit>
-			<HeroOrbit size={550} rotation={20}>
-				<StarIcon className="size-12 text-emerald-300" />
-			</HeroOrbit>
-			<HeroOrbit size={590} rotation={98}>
-				<StarIcon className="size-8 text-emerald-300" />
-			</HeroOrbit>
+				{heroOrbitStarMap.map(({ size, child, rotation }) => (
+					<HeroOrbit
+						key={`${size}-${rotation}`}
+						size={size}
+						rotation={rotation}
+					>
+						{child}
+					</HeroOrbit>
+				))}
 
-			<HeroOrbit size={430} rotation={-14}>
-				<SparkleIcon className="size-8 text-emerald-300/20" />
-			</HeroOrbit>
-			<HeroOrbit size={440} rotation={79}>
-				<SparkleIcon className="size-5 text-emerald-300/20" />
-			</HeroOrbit>
-			<HeroOrbit size={530} rotation={178}>
-				<SparkleIcon className="size-10 text-emerald-300/20" />
-			</HeroOrbit>
-			<HeroOrbit size={710} rotation={144}>
-				<SparkleIcon className="size-14 text-emerald-300/20" />
-			</HeroOrbit>
+				{heroOrbitSparkMap.map(({ size, child, rotation }) => (
+					<HeroOrbit
+						key={`${size}-${rotation}`}
+						size={size}
+						rotation={rotation}
+					>
+						{child}
+					</HeroOrbit>
+				))}
 
-			<HeroOrbit size={720} rotation={85}>
-				<div className="size-3 rounded-full bg-emerald-300/20" />
-			</HeroOrbit>
+				{heroOrbitDotMap.map(({ size, child, rotation }) => (
+					<HeroOrbit
+						key={`${size}-${rotation}`}
+						size={size}
+						rotation={rotation}
+					>
+						{child}
+					</HeroOrbit>
+				))}
+			</div>
 
 			<div className="container">
 				<div className="flex flex-col items-center">
