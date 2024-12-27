3131765016ab2f23f51f8a133c9f126492120675
SimonM <simomor@gmail.com>
2024-12-27 12:39:56 +0200

    nav little fix

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { capitalize } from "lodash";
 import clsx from "clsx";
-import { RoutesSet } from "@/constants/StaticNavigation";
+import { HeaderTypes, RoutesSet } from "@/constants/StaticNavigation";
+import { useState } from "react";
 
 export const Header = () => {
+	const [activeRoute, setActiveRoute] = useState<HeaderTypes>();
+
 	return (
 		<div className="fixed top-3 z-10 flex w-full items-center justify-center">
 			<nav className="flex gap-1 rounded-full border border-white/15 bg-white/10 p-0.5 backdrop-blur">
@@ -11,10 +16,16 @@ export const Header = () => {
 						className={clsx(
 							"nav-item",
 							index === RoutesSet.size - 1 &&
-								"bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+								"bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900",
+							activeRoute === route && "bg-white/20 text-cyan-300"
 						)}
 						key={`${route}-${index}`}
 						href={`#${route}`}
+						onClick={() =>
+							route === "contact"
+								? (window.location.href = `mailto:${process.env.NEXT_PUBLIC_PERSONAL_EMAIL}`)
+								: setActiveRoute(route)
+						}
 					>
 						{capitalize(route)}
 					</a>
