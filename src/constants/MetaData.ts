199b80cf4cbdaffeadcd9dd74acbfb1894736108
SimonM <simomor@gmail.com>
2024-12-30 20:58:21 +0200

    meta

--- src/constants/MetaData.ts
+++ src/constants/MetaData.ts
@@ -18,16 +18,11 @@ export const MetaData: MetadataType = {
 		description: "Simon Mor - Explore my work and projects.",
 		url: "https://simonmor.com",
 		siteName: "SM Portfolio",
-		images: [
-			{
-				url: `https://simonmor.com/favicon.ico`,
-				width: 1200,
-				height: 630,
-				alt: "My Portfolio OG Image"
-			}
-		],
 		locale: "en_US",
-		type: "website"
+		type: "website",
+		determiner: "the",
+		emails: ["simomor@gmail.com"],
+		alternateLocale: ["fr_FR", "he"]
 	},
 	twitter: {
 		card: "summary_large_image",
